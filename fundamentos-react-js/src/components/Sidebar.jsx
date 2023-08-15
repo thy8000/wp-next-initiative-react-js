2ec21e2041821b431b57141ca188161a5339705e
Thunay <thunaymoreiradesoares@gmail.com>
2023-08-15 10:31:18 -0300

    Estrutura da aplicação - Finalizando sidebar

--- fundamentos-react-js/src/components/Sidebar.jsx
+++ fundamentos-react-js/src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import styles from "./Sidebar.module.css";
 
+import { PencilLine } from "phosphor-react";
+
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
@@ -9,13 +11,19 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/thy8000.png" />
+
         <strong>Thunay Moreira</strong>
 
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size="20" />
+
+          Editar seu perfil
+          </a>
       </footer>
     </aside>
   );
