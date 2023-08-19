774abd852657c7ed86fd18c708219253216d1a13
Thunay <thunaymoreiradesoares@gmail.com>
2023-08-19 14:27:45 -0300

    Os motores do React - Iterando no JSX

--- fundamentos-react-js/src/App.jsx
+++ fundamentos-react-js/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
@@ -5,6 +6,57 @@ import { Sidebar } from "./components/Sidebar";
 import styles from "./App.module.css";
 
 import "./global.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/thy8000",
+      name: "Thunay Moreira",
+      role: "Desenvolvedor Fullstack",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/Pnmoura",
+      name: "Pedro Nunes",
+      role: "Desenvolvedor Fullstack",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
 
 function App() {
   return (
@@ -15,15 +67,15 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post
-            author="Jimmy Neutron Africano"
-            content="Lorem ipsum dolor sit amet consectetur adipisincg elit."
-          />
-
-          <Post
-            author="Jimmy Neutron Sueco"
-            content="radh\aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-          />
+          {posts.map(post => {
+            return (
+            <Post 
+            author={post.author}
+            content={post.content}
+            publishedAt={post.publishedAt}
+            />
+            )
+          })}
         </main>
       </div>
     </div>
